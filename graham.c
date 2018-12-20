b5d5ac4a11990472e17f0bd3cec24ca271a709d8
Yahya Bakkali <yahyabakkali@hotmail.com>
2018-12-20 20:15:37 +0100

    add comments

--- graham.c
+++ graham.c
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -18,7 +17,7 @@
 // Le port du serveur où le client va se connecter
 #define MAXDATASIZE 1024
 // Nombre maximum des informations
-#define h_addr h_addr_list[0] 
+#define h_addr h_addr_list[0]
 
 int main(int argc, char *argv[]) {
 
@@ -28,10 +27,12 @@ int main(int argc, char *argv[]) {
     }
 
     struct hostent *he;
-    struct sockaddr_in their_addr; // connector's address information
+    struct sockaddr_in their_addr;
+    // Informations sur l'adresse du connecteur
 
     he=gethostbyname(argv[1]) ;
-    if ( he == NULL) {  // get the host info
+    // Obtenir les infos sur l'hôte
+    if ( he == NULL) {
         perror("Client: gethostbyname");
         return EXIT_FAILURE;
     }
@@ -42,10 +43,13 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    their_addr.sin_family = AF_INET;    // host byte order
-    their_addr.sin_port = htons(PORT);  // short, network byte order
+    their_addr.sin_family = AF_INET;
+    // ordre des octets de l'hôte
+    their_addr.sin_port = htons(PORT);
+    // court, ordre d'octets réseau
     their_addr.sin_addr = *((struct in_addr *)he->h_addr);
-    memset(&(their_addr.sin_zero), '\0', 8);  // zero the rest of the struct
+    memset(&(their_addr.sin_zero), '\0', 8);
+    // zéro pour le reste de la structure
 
     if (connect(sockfd, (struct sockaddr *)&their_addr, sizeof(struct sockaddr)) == -1) {
         perror("Client: connect");
@@ -55,60 +59,64 @@ int main(int argc, char *argv[]) {
     char *filename = strrchr(argv[3],'/')+1;
     int usernameSize = strlen(argv[2]) ;
     int filenameSize = strlen(filename) ;
-    /////////
+
+    /* Envoie de la taille d'utilisateur et du nom de fichier */
+
     if (send(sockfd, &usernameSize ,sizeof(int),0)==-1) {
-        perror("Client: Send error");
+        perror("Client: send error");
         return EXIT_FAILURE;
     }
 
-    printf("Send username size\n") ;
+    printf("Send username size : %d\n",usernameSize) ;
 
     if (send(sockfd, &filenameSize  ,sizeof(int),0)==-1) {
-        perror("Client: Send error");
+        perror("Client: send error");
         return EXIT_FAILURE;
     }
 
-    printf("Send filename size\n") ;
+    printf("Send filename size : %d\n",filenameSize) ;
 
-    //////////
+    /* Envoie du nom d'utilisateur,année,mois,jour,nom du ficher */
+
     if (send(sockfd,argv[2],usernameSize,0)==-1) {
-        perror("Client: Send error");
+        perror("Client: send error");
         return EXIT_FAILURE;
     }
 
-    printf("Send username \n") ;
+    printf("Send username : %s\n",argv[2]) ;
 
     if (send(sockfd,argv[4],4,0)==-1) {
-        perror("Client: Send error");
+        perror("Client: send error");
         return EXIT_FAILURE;
     }
 
-    printf("Send year \n") ;
+    printf("Send year : %s\n",argv[4]) ;
 
     if (send(sockfd,argv[5],2,0)==-1) {
-        perror("Client: Send error");
+        perror("Client: send error");
         return EXIT_FAILURE;
     }
 
-    printf("Send month \n") ;
+    printf("Send month : %s\n",argv[5]) ;
 
     if (send(sockfd,argv[6],2,0)==-1) {
-        perror("Client: Send error");
+        perror("Client: send error");
         return EXIT_FAILURE;
     }
 
-    printf("Send day \n") ;
+    printf("Send day : %s\n",argv[6]) ;
 
     if (send(sockfd,filename,filenameSize,0)==-1) {
-        perror("Client: Send error");
+        perror("Client: send error");
         return EXIT_FAILURE;
     }
 
-    printf("Send filename \n") ;
-////////
+    printf("Send filename : %s\n",filename) ;
+
+    /* Envoie de la taille du fichier et du fichier */
 
     FILE *fptr = fopen(argv[3], "r");
-    // Fichier a envoyer
+    // Ouvrir le fichier à envoyer
     if ( fptr == NULL ) {
        perror("Error : Open file return NULL");
        return EXIT_FAILURE;
@@ -118,30 +126,35 @@ int main(int argc, char *argv[]) {
     rewind(fptr);
 
     if (send(sockfd, &fileSize , sizeof(int) ,0)==-1) {
-      perror("Client: Send error");
+      perror("Client: send error");
       return EXIT_FAILURE;
     }
 
-    printf("Send file size \n") ;
-/////////
+    printf("Send file size : %d\n",fileSize) ;
+
     char buffer[MAXDATASIZE] ;
     int numbytes = 1;
     while ((numbytes = fread(buffer,sizeof(char),MAXDATASIZE,fptr))) {
         if (send(sockfd,buffer,numbytes,0)==-1) {
-            perror("Client: Send error");
+            perror("Client: send error");
             return EXIT_FAILURE;
         }
     }
-/////////
+
+    printf("Send file \n") ;
+
+    /* Réception du code de retour et le traitement du code
+        0 en cas de succès sinon 1 et l'erreur parvenue
+    */
+
     char EXIT_Message[MAXDATASIZE+1] ;
     if ( recv(sockfd, EXIT_Message ,MAXDATASIZE, 0) == -1) {
-        // Reception du code 0 en cas de succès sinon 1 et l'erreur parvenue
-        perror("Client: Recv");
+        perror("Client: recv");
         return EXIT_FAILURE;
     }
-////////
+
     char exitStat = EXIT_Message[0] ;
-    printf("Server Send code %c\n",exitStat );
+    printf("Receive code %c\n",exitStat );
     if ( exitStat != '0' ) {
       // Une erreur est reçue du côte serveur
       fprintf(stderr,"%s\n",EXIT_Message+1);
--- spbx.c
+++ spbx.c
@@ -6,14 +6,12 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netdb.h>
-
 #include <sys/stat.h>
 
 #define PORT 5555
@@ -24,7 +22,7 @@ int makeDirectory(char *dir ,int sockfd) {
 
     /* Fonction pour créer un repertoire */
 
-    int  dirStat ;
+    int dirStat ;
     struct stat st = {0};
 
     if (stat(dir, &st) == -1) {
@@ -38,7 +36,7 @@ int makeDirectory(char *dir ,int sockfd) {
             strcpy(errorMessage,"1Error : mkdir") ;
             if (send(sockfd,errorMessage,MAXDATASIZE,0)==-1) {
                 // Envoyer au client l'erreur parvenue
-                perror("Server: Send");
+                perror("Server: send");
                 return EXIT_FAILURE;
             }
         }
@@ -71,13 +69,14 @@ int makeCopy(char *path , FILE* ftmp , int sockfd) {
     */
 
     rewind(ftmp);
+    // Début de ficher
     FILE *fptr ;
     printf("Moving file to : %s\n",path) ;
     fptr = fopen(path , "w");
     if ( fptr == NULL ) {
         perror("Error : File not create");
         if (send(sockfd, "1Error : File not create" ,MAXDATASIZE,0)==-1) {
-            perror("Server: Send");
+            perror("Server: send");
             return EXIT_FAILURE;
         }
         return EXIT_FAILURE;
@@ -99,21 +98,20 @@ int main(int argc, char *argv[]) {
     }
 
     int server_sockfd, client_sockfd ;
-    // Ecouter sur server_sockfd
-    // Se connecter sur client_sockfd
+    // Ecouter sur server_sockfd et se connecter sur client_sockfd
     struct sockaddr_in server_addr;
     // Information de l'adresse du serveur
     struct sockaddr_in client_addr;
     // Information de l'adresse du client
 
     if ((server_sockfd = socket(PF_INET,SOCK_STREAM, 0))== -1) {
-        perror("Serveur: socket");
+        perror("Server: socket");
         return EXIT_FAILURE;
     }
 
     int yes=1;
     if (setsockopt(server_sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1) {
-        perror("Serveur: setsockopt");
+        perror("Server: setsockopt");
         return EXIT_FAILURE;
     }
 
@@ -124,125 +122,130 @@ int main(int argc, char *argv[]) {
 
 
     if (bind(server_sockfd,(struct sockaddr *)&server_addr,sizeof(struct sockaddr)) == -1){
-        perror("Serveur: bind");
+        perror("Server: bind");
         return EXIT_FAILURE;
     }
 
     if (listen(server_sockfd, BACKLOG) == -1) {
-        perror("Serveur: listen");
+        perror("Server: listen");
         return EXIT_FAILURE;
     }
 
     unsigned int sin_size = sizeof(struct sockaddr_in);
 
     while(1){
-
+        /* le processus du serveur */
         if ((client_sockfd = accept(server_sockfd,(struct sockaddr *)&client_addr,&sin_size)) == -1) {
-            perror("Serveur: accept");
+            perror("Server: accept");
         }
 
-        printf("Serveur:  connection recue du client %s\n",inet_ntoa(client_addr.sin_addr));
+        printf("Server: connection received from the client %s\n",inet_ntoa(client_addr.sin_addr));
 
         if (fork()==0) {
-            /* this is the child process */
+            /* le processus de chaque client */
             close(server_sockfd);
+            // Fermer le socket du serveur
             char errorMessage[MAXDATASIZE] ;
-            int usernameSize ;
-            int filenameSize ;
-            int numbytes ;
-            /////////
+            // Liste pour stocker le message d'erreur
+            int usernameSize, filenameSize, numbytes ;
+            // Variables pour stocker les tailles
+
+            /* Réception de la taille d'utilisateur et du nom de fichier */
+
             if (recv(client_sockfd,&usernameSize,sizeof(int),0) == -1){
-                perror("Server: Recv");
+                perror("Server: recv");
                 return EXIT_FAILURE;
             }
 
-            printf("Recieve username size %d \n",usernameSize) ;
+            printf("Receive username size : %d \n",usernameSize) ;
 
             if (recv(client_sockfd,&filenameSize,sizeof(int),0) == -1) {
-                perror("Server: Recv");
+                perror("Server: recv");
                 return EXIT_FAILURE;
             }
 
-            printf("Recieve filename size %d \n",filenameSize) ;
-            /////////
-            char buffer[MAXDATASIZE+1];
-            // Liste pour stocker les informations
-            char username[usernameSize+1] ;
-            // Liste pour stocker le "username"
-            char year[5] ;
-            // Liste pour stocker le "year"
-            char month[3] ;
-            // Liste pour stocker le "month"
-            char day[3] ;
-            // Liste pour stocker le "day"
-            char filename[filenameSize+1] ;
-            // Liste pour stocker le "filename"
+            printf("Receive filename size : %d \n",filenameSize) ;
 
-            /////////
+            /* Des listes pour stocker les informations :
+                - nom d'utilisateur : taille reçue
+                - année : 4
+                - mois : 2
+                - jour : 2
+                - nom du ficher : taille reçue
+             */
+
+            char username[usernameSize+1] ;
+            char year[5] ;
+            char month[3] ;
+            char day[3] ;
+            char filename[filenameSize+1] ;
+
+            /* Réception du nom d'utilisateur,année,mois,jour,nom du ficher */
+
             if ((numbytes=recv(client_sockfd,username,usernameSize,0)) == -1) {
                 strcpy(errorMessage,"1Error : Username not received\n") ;
                 if (send(client_sockfd,errorMessage,MAXDATASIZE,0)==-1) {
                     // Envoyer au client l'erreur parvenue
-                    perror("Server: Send");
+                    perror("Server: send");
                     return EXIT_FAILURE;
                 }
-                perror("Server: Recv");
+                perror("Server: recv");
                 return EXIT_FAILURE;
             }
             username[numbytes] = '\0';
-            printf("Message recu du client: %s \n",username);
+            printf("Receive username : %s \n",username);
 
             if ( (numbytes=recv(client_sockfd, year, 4 , 0)) == -1) {
                 strcpy(errorMessage,"1Error : Year not received\n") ;
                 if (send(client_sockfd,errorMessage,MAXDATASIZE,0)==-1) {
                     // Envoyer au client l'erreur parvenue
-                    perror("Server: Send");
+                    perror("Server: send");
                     return EXIT_FAILURE;
                 }
-                perror("Server: Recv");
+                perror("Server: recv");
                 return EXIT_FAILURE;
             }
             year[numbytes] = '\0';
-            printf("Message recu du client: %s \n",year);
+            printf("Receive year : %s \n",year);
 
             if ( (numbytes=recv(client_sockfd, month, 2 , 0)) == -1) {
                 strcpy(errorMessage,"1Error : Month not received\n") ;
                 if (send(client_sockfd,errorMessage,MAXDATASIZE,0)==-1) {
                     // Envoyer au client l'erreur parvenue
-                    perror("Server: Send");
+                    perror("Server: send");
                     return EXIT_FAILURE;
                 }
-                perror("Server: Recv");
+                perror("Server: recv");
                 return EXIT_FAILURE;
             }
             month[numbytes] = '\0';
-            printf("Message recu du client: %s \n",month);
+            printf("Receive month : %s \n",month);
 
             if ( (numbytes=recv(client_sockfd, day, 2 , 0)) == -1) {
                 strcpy(errorMessage,"1Error : Day not received\n") ;
                 if (send(client_sockfd,errorMessage,MAXDATASIZE,0)==-1) {
                     // Envoyer au client l'erreur parvenue
-                    perror("Server: Send");
+                    perror("Server: send");
                     return EXIT_FAILURE;
                 }
-                perror("Server: Recv");
+                perror("Server: recv");
                 return EXIT_FAILURE;
             }
             day[numbytes] = '\0';
-            printf("Message recu du client: %s \n",day);
+            printf("Receive day : %s \n",day);
 
             if ( (numbytes=recv(client_sockfd, filename, filenameSize , 0)) == -1) {
                 strcpy(errorMessage,"1Error : Filename not received\n") ;
                 if (send(client_sockfd,errorMessage,MAXDATASIZE,0)==-1) {
                     // Envoyer au client l'erreur parvenue
-                    perror("Server: Send");
+                    perror("Server: send");
                     return EXIT_FAILURE;
                 }
-                perror("Server: Recv");
+                perror("Server: recv");
                 return EXIT_FAILURE;
             }
             filename[numbytes] = '\0';
-            printf("Message recu du client: %s \n",filename);
+            printf("Receive filename : %s \n",filename);
             ////////
 
             char tmpPATH[MAXDATASIZE+1] = "/tmp/" ;
@@ -252,33 +255,33 @@ int main(int argc, char *argv[]) {
             // Pointeur vers le fichier temporaire
             ftmp = fopen(tmpPATH , "w+");
             // Créer le fichier temporaire
-            if ( ftmp == NULL ) {
+            if (ftmp == NULL) {
                 perror("Error : No such file");
                 return EXIT_FAILURE;
             }
             ///////
             int recvFileSize ;
-            // Taille du fichier photo qu'on va recevoir
-            int numbytes_ ;
-            // Numero des bytes reçues
-            if ((numbytes_=recv(client_sockfd,&recvFileSize,sizeof(int),0))== -1) {
-                perror("Server: Recv");
+            // Taille du fichier photo envoyé
+
+            if ((numbytes=recv(client_sockfd,&recvFileSize,sizeof(int),0))== -1) {
+                perror("Server: recv");
                 return EXIT_FAILURE;
             }
+            // Réception de la taille du fichier
+            printf("Receive file size : %d \n",recvFileSize);
             ///////
-
+            char buffer[MAXDATASIZE+1];
+            // Liste pour stocker les informations
             int fileSize = 0 ;
             // La taille finale du fichier photo reçue
-            int numbytes__ ;
-            // Numero des bytes reçues
             while (fileSize < recvFileSize ) {
-                if ((numbytes__=recv(client_sockfd,buffer,MAXDATASIZE,0))==-1) {
+                if ((numbytes=recv(client_sockfd,buffer,MAXDATASIZE,0))==-1) {
                     // Reception interrompue
-                    perror("Server: Recv");
+                    perror("Server: recv");
                     return EXIT_FAILURE;
                 }
-                fwrite (buffer,sizeof(char),numbytes__,ftmp);
-                fileSize += numbytes__ ;
+                fwrite (buffer,sizeof(char),numbytes,ftmp);
+                fileSize += numbytes ;
             }
 
             if (fileSize < recvFileSize) {
@@ -286,23 +289,30 @@ int main(int argc, char *argv[]) {
                 strcpy(errorMessage,"1Error : Damaged file") ;
                 if (send(client_sockfd,errorMessage,MAXDATASIZE,0)==-1) {
                     // Envoyer au client l'erreur parvenue
-                    perror("Server: Send");
+                    perror("Server: send");
                     return EXIT_FAILURE;
                 }
                 return EXIT_FAILURE;
             }
+
+            printf("Receive file\n") ;
             ///////
             char finalPATH[MAXDATASIZE] ;
             char* dir[5] = {username,year,month,day,filename} ;
             makePATH(dir,argv[1],finalPATH,client_sockfd);
+            // Création de l’arborescence
             makeCopy(finalPATH,ftmp,client_sockfd) ;
+            // Enregistrement de la photographie
             fclose(ftmp) ;
+            // Fermer le fichier temporaire
             remove(tmpPATH);
+            // Supprimer le fichier temporaire
+
+            /* Le processus est bien exécuté et retourne le code 0 */
 
             char message[MAXDATASIZE] = {"0"} ;
             if (send(client_sockfd,message,MAXDATASIZE,0)==-1) {
-                // Envoi du code 0 en cas de succès
-                perror("Server: Send");
+                perror("Server: send");
                 return EXIT_FAILURE;
             }
             return EXIT_SUCCESS;
